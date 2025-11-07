72f47e1ead727682b31d0ba8ed22e4fd684a582e
alexm <alexmarica07@gmail.com>
2025-11-07 11:00:36 +0200

    sfml

--- main.cpp
+++ main.cpp
@@ -1,68 +1,25 @@
-#include "Nivel.h"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    // Am scos initializarea rand()
+int main() {
+    // Creează o fereastră
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Test SFML");
 
-    Nivel joc({50, 50}, 25.0f);
-    joc.adaugaBileStart();
+    // O formă simplă pentru test
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    std::cout << "Stare initiala joc:" << std::endl;
-    std::cout << joc << std::endl;
-
-    std::ifstream f("tastatura.txt");
-    if (!f.is_open()) {
-        std::cerr << "EROARE: Nu am putut deschide tastatura.txt!" << std::endl;
-        return 1;
-    }
-
-    std::string linie, comanda;
-    float valoare;
-
-    std::cout << "\n--- Incepe citirea comenzilor din tastatura.txt ---\n";
-    while (std::getline(f, linie))
-    {
-        if (linie.empty() || linie[0] == '#') continue;
-
-        std::stringstream ss(linie);
-        ss >> comanda >> valoare;
-
-        std::cout << "\n>>> COMANDA CITITA: " << comanda << " " << valoare << std::endl;
-
-        joc.proceseazaComanda(comanda, valoare);
-
-        std::cout << joc << std::endl;
-
-        if (joc.isTerminat()) {
-            std::cout << "JOCUL S-A TERMINAT!" << std::endl;
-            break;
+    // Bucla principală a jocului
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
-    }
-    f.close();
-    std::cout << "--- Sfarsit citire comenzi ---\n";
 
-    std::cout << "\nSCOR FINAL: " << joc.getScor() << std::endl;
-
-    std::cout << "\n--- DEMONSTRATIE REGULA CELOR TREI ---\n";
-    {
-        std::cout << "Test Constructor Copiere (s2 = s1)...\n";
-        SirDeBile s1_copie = joc.getSirDeBile();
-
-        std::cout << "Test Operator Atribuire (s3 = s1)...\n";
-        SirDeBile s2_atribuire;
-        s2_atribuire = joc.getSirDeBile();
-
-        // MODIFICAREA pentru a opri eroarea de "unused variable" (-Werror)
-        (void)s1_copie;
-        (void)s2_atribuire;
-
-        std::cout << "Obiectele copie vor fi distruse acum (ies din scope)...\n";
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
 
-    std::cout << "Obiectul original 'joc' va fi distrus acum (ies din main)...\n";
     return 0;
 }
